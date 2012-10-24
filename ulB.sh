7bbe872fc435c5b2a86628740ab056a47d7ccc5e
spark4160 <hineta64@gmail.com>
2012-10-25 09:51:40 +1000

    edit

---
+++ ulB.sh
@@ -0,0 +1,7 @@
+cd src
+nxjc HarvestB.java
+echo "1"
+nxjlink -o HarvestB.nxj HarvestB
+echo "2"
+nxjupload HarvestB.nxj
+echo "3"
---
+++ ulF.sh
@@ -0,0 +1,7 @@
+cd src
+nxjc HarvestF.java
+echo "1"
+nxjlink -o HarvestF.nxj HarvestF
+echo "2"
+nxjupload HarvestF.nxj
+echo "3"

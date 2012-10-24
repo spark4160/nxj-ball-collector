72a3d3bcff9c6c07d8d3505a70012348f009b4d6
spark4160 <hineta64@gmail.com>
2012-10-25 09:59:02 +1000

    mod

--- src/HarvestB.java
+++ src/HarvestB.java
@@ -1,89 +1,101 @@
-// Test
-import lejos.nxt.*;
-import lejos.robotics.objectdetection.*;
-import lejos.util.*;
+import java.io.IOException;
 
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.LightSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+import lejos.nxt.comm.Bluetooth;
+import lejos.nxt.comm.NXTCommConnector;
+import lejos.nxt.remote.RemoteNXT;
+import lejos.robotics.objectdetection.Feature;
+import lejos.robotics.objectdetection.FeatureDetector;
+import lejos.robotics.objectdetection.RangeFeatureDetector;
+import lejos.util.TimerListener;
+import lejos.util.Timer;
 
 public class HarvestB {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		// Spin until ball is found
 		// Go to ball until reached or hit line
 		// If hit line then
-			// Reverse for half a second (?)
-			// Search for next ball
+		// Reverse for half a second (?)
+		// Search for next ball
 		// If reached ball
-			// If ball is dark pick up
-			// Else let it be, reverse for half a second then search for next ball
+		// If ball is dark pick up
+		// Else let it be, reverse for half a second then search for next ball
 		// If the robot spins multiple times without detecting a ball then
 		// move in one direction a bit and search again
 		// if still no balls are present after three movements
 		// then move to edge of circle and end program
-		
+
 		// Ports:
-		// S1: Ultrasonic
-		// S2: IR Down 
+		// NXTF:
+		// MB: Tray
+		// NXTB:
+		// S1: IR Down
+		// S2: Ultrasonic
 		// S3: IR Ball
-		// S4:
 		// MA: Left Wheel
 		// MB: Forklift
-		// MC: Right Wheel		
-		
-		/*
-		Feature hit = null;
-		for(int i = 0; i < 10; i++){
-			hit = scanOnce();
-			if(hit != null){
-				LCD.drawInt((int) hit.getRangeReading().getRange(), 0, 0);
-			}
-			Delay.msDelay(1000);
+		// MC: Right Wheel
+
+		LCD.drawString("Connecting...", 0, 0);
+		if (BTConnect()) {
+			LCD.clear();
+			LCD.drawString("Connected.", 0, 0);
+		} else {
+			LCD.clear();
+			LCD.drawString("Connect fail.", 0, 0);
 		}
-		*/
-		/*/pickUp();
-		Motor.B.forward();
-		Delay.msDelay(500);
-		Motor.B.backward();
-		Delay.msDelay(500);
-		Motor.B.stop();
-		*/
-		lineFollow();
-		/*
-		while(!Button.ESCAPE.isDown()){
-			LCD.drawInt(downLight.getLightValue(), 0, 0);
-		}
-		*/
+		Button.waitForAnyPress();
+		System.exit(1);
 	}
+
+	UltrasonicSensor ultra = new UltrasonicSensor(SensorPort.S2);
+	FeatureDetector feature = new RangeFeatureDetector(ultra, 128, 10);
 	
-	private static UltrasonicSensor ultra = new UltrasonicSensor(SensorPort.S2);
-	private static FeatureDetector feature = new RangeFeatureDetector(ultra, 128, 10);
-	/*feature.addListener(new FeatureListener() {
+	feature.addListener(new FeatureListener() { 
 		public void featureDetected(Feature feature, FeatureDetector detector) {
 			int range = (int) feature.getRangeReading().getRange();
-			LCD.drawInt(range, 0, 0);
+		LCD.drawInt(range, 0, 0); 
 		}
-	});*/
-	
-	private static LightSensor downLight = new LightSensor(SensorPort.S1);
-	private static LightSensor ballLight = new LightSensor(SensorPort.S3);
-	
-	private static Timer stopTimer = new Timer(10000, new TimerListener() {
+	 });
+	 
+
+	private LightSensor downLight = new LightSensor(SensorPort.S1);
+	private LightSensor ballLight = new LightSensor(SensorPort.S3);
+
+	private static NXTCommConnector connector;
+	private static RemoteNXT NXTF;
+
+	private Timer stopTimer = new Timer(10000, new TimerListener() {
 		public void timedOut() {
 			Motor.A.stop();
 			Motor.C.stop();
 		}
 	});
-	
-	private static Timer armTimer = new Timer(500, new TimerListener() {
+
+	private Timer armTimer = new Timer(500, new TimerListener() {
 		public void timedOut() {
 			Motor.B.stop();
 			armTimer.stop();
 		}
 	});
-	
-	private static Feature scanOnce() {
+
+	private static Timer trayTimer = new Timer(200, new TimerListener() {
+		public void timedOut() {
+			NXTF.B.stop();
+			trayTimer.stop();
+		}
+	});
+
+	private Feature scanOnce() {
 		Feature hit = null;
 		Motor.A.setSpeed(90);
 		Motor.C.setSpeed(90);
@@ -91,9 +103,9 @@ public class HarvestB {
 		stopTimer.start();
 		Motor.A.forward();
 		Motor.C.backward();
-		while(spin && Motor.A.isMoving() && Motor.C.isMoving()){
+		while (spin && Motor.A.isMoving() && Motor.C.isMoving()) {
 			hit = feature.scan();
-			if(hit != null){
+			if (hit != null) {
 				spin = false;
 			}
 		}
@@ -102,13 +114,13 @@ public class HarvestB {
 		stopTimer.stop();
 		return hit;
 	}
-	
-	private static void pickUp() {
+
+	private void pickUp() {
 		Motor.B.forward();
 		armTimer.start();
 	}
-	
-	private static int lineFollow() {
+
+	private int lineFollow() {
 		// 0 = end of line?, 1 = yellow block, 2 = blue block
 		int ret = 0;
 		boolean follow = true, left = true;
@@ -116,12 +128,12 @@ public class HarvestB {
 		Motor.C.setSpeed(200);
 		Motor.C.stop();
 		Motor.A.forward();
-		while(!Button.ESCAPE.isDown()){
-			if(left && downLight.getLightValue() > 50){
+		while (!Button.ESCAPE.isDown()) {
+			if (left && downLight.getLightValue() > 50) {
 				left = false;
 				Motor.C.stop();
 				Motor.A.forward();
-			}else if(!left && downLight.getLightValue() < 50){
+			} else if (!left && downLight.getLightValue() < 50) {
 				left = true;
 				Motor.A.stop();
 				Motor.C.forward();
@@ -130,4 +142,18 @@ public class HarvestB {
 		return ret;
 	}
 
+	private static void dumpTray() {
+		NXTF.B.forward();
+		trayTimer.start();
+	}
+
+	private static boolean BTConnect() {
+		try {
+			connector = Bluetooth.getConnector();
+			NXTF = new RemoteNXT("NXTCHEES", connector);
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
 }
